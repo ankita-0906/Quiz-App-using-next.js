fb93e4cb10f882c25ad2254a3ce2f1cb62eb16f9
ankita-0906 <ankita20040625@gmail.com>
2024-12-18 15:05:40 +0530

    updated api url:

--- app/frontend/Quiz.js
+++ app/frontend/Quiz.js
@@ -15,7 +15,7 @@ export default function Quiz() {
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
-        const res = await fetch("https://quiz-app-using-next-js.vercel.app/api/backend/firebase");
+        const res = await fetch("/api/backend/firebase");
         const data = await res.json();
         setQuestions(data);
       } catch (error) {
